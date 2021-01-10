e8b4dd98b71f94e1e0b405a15bfbd611211eb4d1
Greg Panciera <gregpanciera@gmail.com>
2021-01-10 13:21:37 -0600

    cors origin change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: true }));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
