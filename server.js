0b9a9ca6eb64310ce6e76707fa2784fc87488713
Greg Panciera <gregpanciera@gmail.com>
2021-01-10 13:03:32 -0600

    cors enabled on server

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
 const server = app.listen(app.get('port'), () => {
-  console.log('listening on port ', server.address().port);
+  console.log('CORS-enabled web server listening on port:', server.address().port);
 });
